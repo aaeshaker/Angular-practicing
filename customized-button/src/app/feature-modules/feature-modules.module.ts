9aedbaf45bbbc8355a5f2b6224c4a2071e54560e
aaeshaker <aaeshaker@outlook.com>
2023-12-13 01:15:59 +0200

    customized button practice

---
+++ customized-button/src/app/feature-modules/feature-modules.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeModule } from './home/home.module';
+import { AboutModule } from './about/about.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HomeModule,
+    AboutModule
+  ]
+})
+export class FeatureModulesModule { }
